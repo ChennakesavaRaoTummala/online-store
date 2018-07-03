7d2a4daa20f6f7eab870661f212af295bb970ebb
Chenna <tummalchenna@gmail.com>
2018-07-04 01:35:35 +0530

    added jekins file

---
+++ src/main/java/com/apple/store/repository/ShipmentRepository.java
@@ -0,0 +1,15 @@
+package com.apple.store.repository;
+
+import com.apple.store.domain.Shipment;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Shipment entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+}
